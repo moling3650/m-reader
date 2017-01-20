b2acf8cc149f0c80e5b0e5c3c2cec47e09e910d1
moling3650 <365024424@qq.com>
2017-01-20 14:02:52 +0800

    编写util库，提供模拟ajax的api接口

---
+++ src/common/js/util.js
@@ -0,0 +1,34 @@
+'use strict'
+const ERR_OK = 0
+
+const getBSONP = (url, callback) => {
+  return window.$.jsonp({
+    url,
+    cache: true,
+    callback: 'duokan_fiction_chapter',
+    success (result) {
+      let data = window.$.base64.decode(result)
+      let json = JSON.parse(decodeURIComponent(escape(data)))
+      callback && callback(json)
+    }
+  })
+}
+
+exports.getChapters = (callback) => {
+  window.$.get('/api/chapter', (data) => {
+    if (data.result === ERR_OK) {
+      callback && callback(data)
+    }
+  }, 'json')
+}
+
+exports.getChapterContent = (id, callback) => {
+  window.$.get(`api/data?id=${id}`, (data) => {
+    if (data.result === ERR_OK) {
+      let url = data.jsonp
+      getBSONP(url, (data) => {
+        callback && callback(data)
+      })
+    }
+  }, 'json')
+}
